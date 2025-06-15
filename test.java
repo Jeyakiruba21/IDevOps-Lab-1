481464e6bdd7bfaf565f5c556cd7dd14caaa96f5
Jeyakiruba <77526217+Jeyakiruba21@users.noreply.github.com>
2025-06-15 19:30:43 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is a test for the Java branch in my IDevOps-Lab-1")
+        System.out.println("This is a test for the Java branch in my IDevOps-Lab-1\n")
+            System.out.println("For SonarQube Scanner")
     }
 }
 
